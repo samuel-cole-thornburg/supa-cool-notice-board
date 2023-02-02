3eac2fddee840290789881cefc8e96dee0d9c22a
samuel-cole-thornburg <96396253+samuel-cole-thornburg@users.noreply.github.com>
2023-02-01 18:59:50 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference=0;
+rightX = 0;
+leftX = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550,500);
@@ -10,6 +15,12 @@ function setup() {
 }
 function draw() {
     background('#4682b4')
+
+    document.getElementById("square").innerHTML = "W and H of sqare is:" + difference +"px";
+    textSize(difference);
+     fill('#FFE787');
+     text('Samuel', 50, 400);
+
 }
 function modelLoaded() {
     console.log('poseNet Is cool')
@@ -19,5 +30,13 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log("results")
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("1:" + noseX + "2:" + noseY);
+        leftX = results[0].pose.leftWrist.x
+        rightX = results[0].pose.rightWrist.x
+        difference = floor(leftX - rightX);
+
+        console.log(leftX + rightX + difference);
     }
 }
